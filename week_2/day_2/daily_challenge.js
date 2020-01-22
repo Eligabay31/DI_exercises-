90abbf85eee79e5c5ff185140831a9b18fa4f762
Eligabay31 <59237195+Eligabay31@users.noreply.github.com>
2020-01-22 16:26:22 +0200

    daily challegne edited

--- week_2/day_2/daily_challenge.js
+++ week_2/day_2/daily_challenge.js
@@ -1,14 +1,17 @@
-// var array = ["banana","apples","oranges","blueberries"];
-// array.shift();
-// console.log(array);
-// array.sort();
-// console.log(array);
-// array.push("kiwi");
-// console.log(array);
-// array.shift();
-// console.log(array);
-// array.reverse();
-// console.log(array);
-
+// daily challenge day_2:
+// part1:
+var array = ["banana","apples","oranges","blueberries"];
+array.shift();
+console.log(array);
+array.sort();
+console.log(array);
+array.push("kiwi");
+console.log(array);
+array.shift();
+console.log(array);
+array.reverse();
+console.log(array);
+// ------------------
+// part2:
 var array2 = ["banana",["apples",["oranges"],"blueberries"]];
 console.log(array2[0][0][0]);

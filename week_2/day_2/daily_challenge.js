67c09160590b5499327c03b79ba2ae44f407ba0e
Eli Gabay <elig@Elis-MacBook-Pro.local>
2020-01-22 16:21:11 +0200

    pushed daily challenge

---
+++ week_2/day_2/daily_challenge.js
@@ -0,0 +1,14 @@
+// var array = ["banana","apples","oranges","blueberries"];
+// array.shift();
+// console.log(array);
+// array.sort();
+// console.log(array);
+// array.push("kiwi");
+// console.log(array);
+// array.shift();
+// console.log(array);
+// array.reverse();
+// console.log(array);
+
+var array2 = ["banana",["apples",["oranges"],"blueberries"]];
+console.log(array2[0][0][0]);
--- week_2/day_2/script.js
+++ week_2/day_2/script.js
@@ -1,16 +1,19 @@
-var array = ["banana","apples","oranges","blueberries"];
-array.shift();
-console.log(array);
-array.sort();
-console.log(array);
-array.push("kiwi");
-console.log(array);
-array.shift();
-console.log(array);
-array.reverse();
-console.log(array);
+// var array = ["banana","apples","oranges","blueberries"];
+// array.shift();
+// console.log(array);
+// array.sort();
+// console.log(array);
+// array.push("kiwi");
+// console.log(array);
+// array.shift();
+// console.log(array);
+// array.reverse();
+// console.log(array);
 
-var array2 
+var array2 = ["banana",["apples",["oranges"],"blueberries"]];
+console.log(array2[1][1][0]);
+
+
 
 // var obj = {
 // 	username: "eli",
